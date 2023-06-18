980920ee72aa80e070c58b6d86b71b8925849cbc
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-06-18 20:25:26 +0500

    v-for list rendering

--- main.js
+++ main.js
@@ -4,9 +4,15 @@ const app = Vue.createApp({
       product: "Socks",
       image: "./assets/images/socks_green.jpg",
       url: "https://www.vuemastery.com/",
-      inStock:false,
-      inventory:0 ,
-      onSale:true
+      inStock: false,
+      inventory: 0,
+      onSale: true,
+      details: ["50% cotton", "30% wool", "20% polyester"],
+      variants: [
+        { id: 1, color: "green" },
+        { id: 2, color: "blue" },
+      ],
+      sizes: ["S", "M", "L", "XL"],
     };
   },
 });
