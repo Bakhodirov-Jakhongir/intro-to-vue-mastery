f24a8954c7afda56743894b20620b508b9ce70de
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-06-18 19:03:49 +0500

    update vue to vue3 and create vue app instance

--- main.js
+++ main.js
@@ -1,33 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product:"Something new",
-            image:'./assets/images/socks_green.jpg',
-            inStock:false,
-            cart:0,
-            details:['80% cotton' , '20% polyester' , "Gender-neutral"],
-            variants:[
-                {
-                    variantId:2234,
-                    variantColor:'green',
-                    variantImage:'./assets/images/socks_green.jpg',
-                },
-                {
-                    variantId:2235,
-                    variantColor:'blue',
-                    variantImage:'./assets/images/socks_blue.jpg'
-                }
-            ]
-        }
-    },
-    methods: {
-        addCart() {
-            this.cart+=1;
-        },
-        updateProduct(variantImage) {
-            this.image = variantImage
-        }
-    },
-})
+  data() {
+    return {
+      product: "Boots",
+      description: "Description about boots",
+    };
+  },
+});
 
-app.mount('#app');
+app.mount("#app");
