25bc4af91c080a19e31cacbe0d89465048808340
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2023-06-21 09:16:50 +0500

    computed property

--- main.js
+++ main.js
@@ -3,16 +3,25 @@ const app = Vue.createApp({
     return {
       cart: 0,
       product: "Socks",
-      image: "./assets/images/socks_green.jpg",
+      brand: "Vue Mastery",
+      selectedVariant: 0,
       url: "https://www.vuemastery.com/",
-      inStock: false,
-      outOfStockImage:'./assets/images/out-of-stock-img.jpg',
       inventory: 10,
       onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
-        { id: 1, color: "green", image: "./assets/images/socks_green.jpg" },
-        { id: 2, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        {
+          id: 1,
+          color: "green",
+          image: "./assets/images/socks_green.jpg",
+          quantity: 20,
+        },
+        {
+          id: 2,
+          color: "blue",
+          image: "./assets/images/socks_blue.jpg",
+          quantity: 1,
+        },
       ],
     };
   },
@@ -25,8 +34,20 @@ const app = Vue.createApp({
         this.cart--;
       }
     },
-    updateImage(variantImage) {
-      this.image = variantImage;
+    updateProductVariant(index) {
+      this.selectedVariant = index;
+      console.log(index);
+    },
+  },
+  computed: {
+    title() {
+      return this.brand + " " + this.product;
+    },
+    image() {
+      return this.variants[this.selectedVariant].image;
+    },
+    inStock() {
+      return this.variants[this.selectedVariant].quantity;
     },
   },
 });
